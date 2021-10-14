9eaf1b7f2e344567c8e113ee66d54deab91a8b75
Olena Honcharuk <gasmin07@gmail.com>
2021-10-15 01:35:09 +0300

    feat: add date and time

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,12 +1,40 @@
 "use strict";
 const time = document.querySelector(".time");
-
+const dayOfWeek = document.querySelector(".date");
 const date = new Date();
-console.log(date);
 const currentTime = date.toLocaleTimeString();
-console.log(currentTime);
+const options = {
+  month: "long",
+  day: "numeric",
+};
+const currentDate = date.toLocaleDateString("en-US", options);
+
+function getWeekDay(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[date.getDay()];
+}
+getWeekDay(date);
+const currentDayOfWeek = getWeekDay(date);
 function showTime() {
   time.textContent = currentTime;
+
+  function showTime() {
+    dayOfWeek.textContent = currentDayOfWeek + "," + " " + currentDate;
+  }
   setTimeout(showTime, 1000);
 }
 showTime();
+
+console.log(date);
+console.log(currentTime);
+console.log(currentDate);
+console.log(getWeekDay(date));
