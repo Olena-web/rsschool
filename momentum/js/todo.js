0446b593903f91d145f7d2e025aa42b3fff4a0c0
Olena Honcharuk <gasmin07@gmail.com>
2021-10-24 16:59:04 +0300

    feat: add showPicture from sources, style for todo

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -6,19 +6,18 @@ todoBtn.addEventListener("click", () => {
   todoBlock.classList.toggle("visible");
 });
 // Vanila JavaScript todo list app
-var taskInput = document.getElementById("new-task"); // New-task
-var addButton = document.getElementsByTagName("button")[0]; // First button
-var incompleteTasksHolder = document.getElementById("incomplete-tasks"); // Incomplete-tasks
-var completedTasksHolder = document.getElementById("completed-tasks"); // Completed-tasks
-
-var createNewTaskElement = function (taskString) {
+const taskInput = document.getElementById("new-task"); // New-task
+const addButton = document.getElementsByTagName("button")[5]; // First button
+const incompleteTasksHolder = document.getElementById("incomplete-tasks"); // Incomplete-tasks
+const completedTasksHolder = document.getElementById("completed-tasks"); // Completed-tasks
+const createNewTaskElement = function (taskString) {
   // New Task List Item
-  var listItem = document.createElement("li"); // Create List Item
-  var checkBox = document.createElement("input"); // Input (checkbox)
-  var label = document.createElement("label"); // Label
-  var editInput = document.createElement("input"); // Input (text)
-  var editButton = document.createElement("button"); // Button.edit
-  var deleteButton = document.createElement("button"); // Button.delete
+  const listItem = document.createElement("li"); // Create List Item
+  const checkBox = document.createElement("input"); // Input (checkbox)
+  const label = document.createElement("label"); // Label
+  const editInput = document.createElement("input"); // Input (text)
+  const editButton = document.createElement("button"); // Button.edit
+  const deleteButton = document.createElement("button"); // Button.delete
 
   checkBox.type = "checkbox"; // Each element needs to be modified
   editInput.type = "text"; //
@@ -37,7 +36,7 @@ var createNewTaskElement = function (taskString) {
   return listItem;
 };
 
-var addTask = function () {
+const addTask = function () {
   // Add a new task
   var listItemName = taskInput.value || "New Item"; // We hold the current value or provide the default one
   var listItem = createNewTaskElement(listItemName); // Create a new list item with the text from #new-task
@@ -46,7 +45,7 @@ var addTask = function () {
   taskInput.value = ""; // Resets the field
 };
 
-var editTask = function () {
+const editTask = function () {
   // Edit an existing task
   var listItem = this.parentNode; // Create List Item
   var editInput = listItem.querySelector("input[type=text"); // Input (text)
@@ -66,7 +65,7 @@ var editTask = function () {
   listItem.classList.toggle("editMode"); // Toggle .editMode on the parent
 };
 
-var deleteTask = function () {
+const deleteTask = function () {
   // Delete an existing task
   var listItem = this.parentNode; // We use parentNode to target the object containing the delete button
   var ul = listItem.parentNode; // We use parentNode again to target the list containing the task
@@ -80,14 +79,14 @@ var taskCompleted = function () {
   bindTaskEvents(listItem, taskIncomplete); // We bind it to the opposite holder
 };
 
-var taskIncomplete = function () {
+const taskIncomplete = function () {
   // Mark a task as incomplete
   var listItem = this.parentNode; // We assign it for readability
   incompleteTasksHolder.appendChild(listItem); // Append the task list item to the #incomplete-tasks
   bindTaskEvents(listItem, taskCompleted); // We bind it to the opposite holder
 };
 
-var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   // Select it's children
   var checkBox = taskListItem.querySelector("input[type=checkbox]"); //
   var editButton = taskListItem.querySelector("button.edit"); //
@@ -98,18 +97,33 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
 };
 
 var ajaxRequest = function () {
-  console.log("AJAX request");
+  // console.log("AJAX request");
 };
-
 addButton.addEventListener("click", addTask); // Adds event listener for the click handler to the addTask function
 addButton.addEventListener("click", ajaxRequest); // Adds an event listener for AJAX
 
-for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
+function setLocalStorage() {
+  localStorage.setItem("task", incompleteTasksHolder.value);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+function getLocalStorage() {
+  if (localStorage.getItem("task")) {
+    incompleteTasksHolder.value = localStorage.getItem(
+      "task",
+      incompleteTasksHolder.value
+    );
+  }
+  // console.log(incompleteTasksHolder.value);
+}
+getLocalStorage();
+window.addEventListener("load", getLocalStorage);
+
+for (let i = 0; i < incompleteTasksHolder.children.length; i++) {
   // Cycle over incompleteTasksHolder ul list items
   bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted); // Bind events to list item's children (taskCompleted)
 }
 
-for (var i = 0; i < completedTasksHolder.children.length; i++) {
+for (let i = 0; i < completedTasksHolder.children.length; i++) {
   // Cycle over completedTasksHolder ul list items
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete); // Bind events to list item's children (taskIncomplete)
 }
