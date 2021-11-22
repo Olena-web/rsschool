f87755f4f29149f753c43f0a677fba2f4b7d8aab
Olena Honcharuk <gasmin07@gmail.com>
2021-11-22 23:37:06 +0200

    feat: add responsive layout

--- art-quiz/js/settings.js
+++ art-quiz/js/settings.js
@@ -4,31 +4,31 @@ const volumeRange = document.querySelector('.volume_range');
 const timeRange = document.querySelector('.time_range');
 const muteButton = document.querySelector('.volume_off');
 const audioElement = document.createElement('audio');
-// const volume = document.getElementById('volume');
+
 function playBtn() {
   audioElement.setAttribute('src', 'sounds/button.wav');
   audioElement.play();
 }
+function forInput() {
+  let volume = audioElement.volume;
+  const value = this.value;
+  volume = value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+  if (volume === 0) {
+    muteButton.style.opacity = 0.4;
+  } else {
+    muteButton.style.opacity = 1;
+  }
+}
 function settingsPage() {
   document.addEventListener('DOMContentLoaded', () => {
     let progress = document.querySelectorAll('.progress');
-
     progress.forEach((elem) => {
-      elem.addEventListener('input', function () {
-        let volume = audioElement.volume;
-        const value = this.value;
-        volume = value;
-        this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
-        if (volume === 0) {
-          muteButton.style.opacity = 0.4;
-        } else {
-          muteButton.style.opacity = 1;
-        }
-      });
+      elem.addEventListener('input', forInput);
     });
 
     labelVolume.addEventListener('click', () => {
-      volumeRange.classList.toggle('non-active');
+      progress[0].classList.toggle('non-active');
       playBtn();
     });
     labelTime.addEventListener('click', () => {
