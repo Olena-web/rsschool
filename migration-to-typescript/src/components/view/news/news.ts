eada337d462519a6caa6351f4b3eec5875d14835
Olena Honcharuk <gasmin07@gmail.com>
2021-12-05 13:29:21 +0200

    refactor: news and sources with generics

--- migration-to-typescript/src/components/view/news/news.ts
+++ migration-to-typescript/src/components/view/news/news.ts
@@ -22,36 +22,36 @@ class News {
         : data;
 
     const fragment = document.createDocumentFragment();
-    const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
-
-    news.forEach((item, idx) => {
-      const newsClone = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
-
-      if (idx % 2) {
-        const newsItem = newsClone.querySelector('.news__item') as HTMLDivElement;
-        newsItem.classList.add('alt');
-        const metaPhoto = newsClone.querySelector('.news__meta-photo') as HTMLDivElement;
-        const author = newsClone.querySelector('.news__meta-author') as HTMLDivElement;
-        const news = document.querySelector('.news') as HTMLDivElement;
-        const title = newsClone.querySelector('.news__description-title') as HTMLTitleElement;
-        const source = newsClone.querySelector('.news__description-source') as HTMLDivElement;
-        const content = newsClone.querySelector('.news__description-content') as HTMLDivElement;
-        const anchor = newsClone.querySelector('.news__read-more a') as HTMLAnchorElement;
-        const metaData = newsClone.querySelector('.news__meta-date') as HTMLDivElement;
-
-        author.textContent = item.author || item.source.name;
-        news.innerHTML = '';
-        title.textContent = item.title;
-        source.textContent = item.source.name;
-        content.textContent = item.description;
-        anchor.setAttribute('href', item.url);
-        news.appendChild(fragment);
-        metaData.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
-        metaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+    const newsItemTemp = document.querySelector<HTMLTemplateElement>('#newsItemTemp');
+    if (newsItemTemp !== null) {
+      news.forEach((item, idx) => {
+        const newsClone = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+        const newsItem = newsClone.querySelector<HTMLDivElement>('.news__item');
+        if (idx % 2 && newsItem !== null) newsItem.classList.add('alt');
+        const metaPhoto = newsClone.querySelector<HTMLDivElement>('.news__meta-photo');
+        const author = newsClone.querySelector<HTMLDivElement>('.news__meta-author');
+        const title = newsClone.querySelector<HTMLTitleElement>('.news__description-title');
+        const source = newsClone.querySelector<HTMLDivElement>('.news__description-source');
+        const content = newsClone.querySelector<HTMLDivElement>('.news__description-content');
+        const anchor = newsClone.querySelector<HTMLAnchorElement>('.news__read-more a');
+        const metaData = newsClone.querySelector<HTMLDivElement>('.news__meta-date');
+        if (author !== null) author.textContent = item.author || item.source.name;
+        if (title !== null) title.textContent = item.title;
+        if (source !== null) source.textContent = item.source.name;
+        if (content !== null) content.textContent = item.description;
+        if (anchor !== null) anchor.setAttribute('href', item.url);
+        if (metaData !== null) metaData.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+        if (metaPhoto !== null)
+          metaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
 
         fragment.append(newsClone);
+      });
+      const newsBlock = document.querySelector<HTMLDivElement>('.news');
+      if (newsBlock !== null) {
+        newsBlock.innerHTML = '';
+        newsBlock.appendChild(fragment);
       }
-    });
+    }
   }
 }
 
--- migration-to-typescript/src/components/view/sources/sources.ts
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -24,24 +24,28 @@ export interface APISource {
   articles?: Article[];
   sources?: Source[];
 }
-
 class Sources {
   draw(data: APISource[]) {
     const fragment = document.createDocumentFragment();
-    const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
-
-    data.forEach((item) => {
-      const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
-      if (sourceClone !== null) {
-        const itemName = sourceClone.querySelector('.source__item-name') as HTMLSpanElement;
-        const itemSource = sourceClone.querySelector('.source__item') as HTMLDivElement;
-        itemName.textContent = item.name;
-        itemSource.setAttribute('data-source-id', item.id);
-        fragment.append(sourceClone);
-      }
-    });
-    const sources = document.querySelector('.sources') as HTMLDivElement;
-    if (sources.textContent == '') {
+    const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp');
+    if (sourceItemTemp !== null) {
+      data.forEach((item) => {
+        const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+        if (sourceClone !== null) {
+          const itemName = sourceClone.querySelector<HTMLSpanElement>('.source__item-name');
+          const itemSource = sourceClone.querySelector<HTMLDivElement>('.source__item');
+          if (itemName !== null) {
+            itemName.textContent = item.name;
+          }
+          if (itemSource !== null) {
+            itemSource.setAttribute('data-source-id', item.id);
+          }
+          fragment.append(sourceClone);
+        }
+      });
+    }
+    const sources = document.querySelector<HTMLDivElement>('.sources');
+    if (sources !== null && sources.textContent == '') {
       sources.append(fragment);
     }
   }
