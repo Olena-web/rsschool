e28ab954eb774f429d9ea0a936a3108cc3b23978
Olena Honcharuk <gasmin07@gmail.com>
2021-12-05 23:52:10 +0200

    feat: migrate loader to TS

--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -12,13 +12,16 @@ class Loader {
       console.error('No callback for GET response');
     }
   ) {
-    this.load('GET', endpoint, callback, options);
+    if (options !== undefined) this.load('GET', endpoint, callback, options);
   }
 
   errorHandler(res: Response): Response {
     if (!res.ok) {
-      if (res.status === 401 || res.status === 404)
+      if (res.status === 401 || res.status === 404) {
         console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      } else if (res.status === 426) {
+        console.log(`Sorry, but today you made 100 requests error: ${res.statusText}`);
+      }
       throw Error(res.statusText);
     }
 
@@ -36,8 +39,9 @@ class Loader {
     return url.slice(0, -1);
   }
 
-  load(method: string, endpoint: string, callback: (data: []) => void, options = {}) {
+  load(method: string, endpoint: string, callback: <T>(data: T) => void, options: Record<string, string>) {
     fetch(this.makeUrl(options, endpoint), { method })
+      // eslint-disable-next-line @typescript-eslint/unbound-method
       .then(this.errorHandler)
       .then((res) => res.json())
       .then((data) => callback(data))
