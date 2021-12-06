2997ba39269a19f085718cc04cb57da1723c456e
Olena Honcharuk <gasmin07@gmail.com>
2021-12-06 15:37:18 +0200

    fix:bug in loader

--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -1,3 +1,8 @@
+enum Errors {
+  'res.status === 401' = 0,
+  'res.status === 404' = 1,
+  'res.status === 426' = 2,
+}
 class Loader {
   baseLink: string;
   options: { [key: string]: string };
@@ -12,16 +17,12 @@ class Loader {
       console.error('No callback for GET response');
     }
   ) {
-    if (options !== undefined) this.load('GET', endpoint, callback, options);
+    this.load('GET', endpoint, callback, options);
   }
 
   errorHandler(res: Response): Response {
     if (!res.ok) {
-      if (res.status === 401 || res.status === 404) {
-        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-      } else if (res.status === 426) {
-        console.log(`Sorry, but today you made 100 requests error: ${res.statusText}`);
-      }
+      if (Errors) console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
       throw Error(res.statusText);
     }
 
@@ -39,7 +40,7 @@ class Loader {
     return url.slice(0, -1);
   }
 
-  load(method: string, endpoint: string, callback: <T>(data: T) => void, options: Record<string, string>) {
+  load(method: string, endpoint: string, callback: <T>(data: T) => void, options = {}) {
     fetch(this.makeUrl(options, endpoint), { method })
       // eslint-disable-next-line @typescript-eslint/unbound-method
       .then(this.errorHandler)
