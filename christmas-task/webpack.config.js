2c5b9ad99cad2ddae6a51f386a2c3036577754e1
Olena Honcharuk <gasmin07@gmail.com>
2021-12-08 22:51:06 +0200

    init: start task

---
+++ christmas-task/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/i,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        // transpileOnly: true
+                    }
+                },
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:mp3|wav|ogg|mp4)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
