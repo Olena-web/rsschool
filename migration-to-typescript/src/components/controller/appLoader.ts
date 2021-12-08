2e87d2e0963081038b2aae81024548f85fa66424
Olena Honcharuk <gasmin07@gmail.com>
2021-12-08 22:05:34 +0200

    refactor: change link for deploy

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'cf49ab624cac4ede9eecf9c7e9efa65a', // получите свой ключ https://newsapi.org/
     });
   }
