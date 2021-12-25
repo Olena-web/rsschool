a1813019084f44ef446a9cc0a3987a60ef2fdd09
Olena Honcharuk <gasmin07@gmail.com>
2021-12-25 18:22:22 +0200

    feat: add change tree

--- christmas-task/src/app/treePage.ts
+++ christmas-task/src/app/treePage.ts
@@ -2,7 +2,9 @@
 const treeItem = document.querySelectorAll<HTMLDivElement>('.menu-tree_item');
 const bgItem = document.querySelectorAll<HTMLDivElement>('.menu-bg__item');
 const mainTree = document.querySelector<HTMLDivElement>('.tree-page__main');
+const treeConytainer = document.querySelector<HTMLDivElement>('.main-tree__container');
 let bgNum;
+
 const img = new Image();
 img.src = `../assets/bg/5.jpg`;
 img.addEventListener('load', () => {
@@ -16,6 +18,21 @@ if (bgItem !== null && mainTree !== null) {
     img.src = `../assets/bg/${bgNum}.jpg`;
     bgItem[i].addEventListener('click', () => {
       if (mainTree) mainTree.style.backgroundImage = `url(${img.src})`;
+    });
+  }
+}
+
+if (treeItem !== null && mainTree !== null) {
+  for (let i = 0; i < treeItem.length; i++) {
+    bgNum = i + 1;
+    const img = new Image();
+    img.src = `../assets/tree/${bgNum}.png`;
+    treeItem[i].addEventListener('click', () => {
+      if (treeConytainer !== null) {
+        //treeConytainer.style.backgroundImage = `url(${img.src})`;
+        treeConytainer.innerHTML = '';
+        treeConytainer.appendChild(img);
+      }
     });
   }
 }
